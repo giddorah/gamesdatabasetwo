ece362d6152a5a09c0c6babc369b28e44dcba3c2
Ludwig Kleiser <ludde_orby@hotmail.com>
2018-01-02 11:11:52 +0100

    Moved database operations to a partial class

---
+++ gamesdatabasetwo/Data/ApplicationDbContextPartial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gamesdatabasetwo.Data
+{
+    public partial class ApplicationDbContextPartial : IdentityDbContext<ApplicationUser>
+    {
+
+        public DbSet<Game> Games { get; set; }
+
+
+        public void Remove(int id)
+        {
+            var gameToRemove = ById(id);
+            Games.Remove(gameToRemove);
+
+            SaveChanges();
+        }
+
+
+        public void AddCustomer(Game game)
+        {
+            Games.Add(game);
+            SaveChanges();
+        }
+        public Game ById(int id)
+        {
+            var gameToReturn = Games.Single(i => i.Id == id);
+
+            return gameToReturn;
+        }
+    }
+}
