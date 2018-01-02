99f70545e23b2d169d6188a3daa5e81505babcb4
Ludwig Kleiser <ludde_orby@hotmail.com>
2018-01-02 10:51:34 +0100

    Revert "Database functions"
    This reverts commit 59c07d29433c10002646b40503c436af534f5c0b.

--- gamesdatabasetwo/Data/ApplicationDbContext.cs
+++ gamesdatabasetwo/Data/ApplicationDbContext.cs
@@ -18,26 +18,5 @@ namespace gamesdatabasetwo.Data
         {
             base.OnModelCreating(builder);
         }
-
-        //public void Remove(int id)
-        //{
-        //    var gameToRemove = ById(id);
-        //    Game.Remove(userToRemove);
-
-        //    SaveChanges();
-        //}
-
-
-        //public void AddCustomer(Game game)
-        //{ 
-        //    Game.Add(game);
-        //    SaveChanges();
-        //}
-        //public Customer ById(int id)
-        //{
-        //    var customerToReturn = Customer.Single(i => i.Id == id);
-
-        //    return customerToReturn;
-        //}
     }
 }
