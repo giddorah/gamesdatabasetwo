cff5f9fb2e0d36174ba71c892bfa1d4e0ad69482
Ludwig Kleiser <ludde_orby@hotmail.com>
2018-01-02 12:08:43 +0100

    Revert "Added user functions for signin, signout and remove"
    This reverts commit 547f4dd9777d50df9bede5e3ffde3e8aae3c5b9d.

--- gamesdatabasetwo/Controllers/UsersController.cs
+++ gamesdatabasetwo/Controllers/UsersController.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using gamesdatabasetwo.Data;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gamesdatabasetwo.Controllers
@@ -12,48 +9,11 @@ namespace gamesdatabasetwo.Controllers
     [Route("api/Users")]
     public class UsersController : Controller
     {
-        private readonly UserManager<ApplicationUser> userManager;
-        private readonly SignInManager<ApplicationUser> signInManager;
-        private readonly RoleManager<IdentityRole> roleManager;
-        private readonly ApplicationDbContext applicationDbContext;
-
-        public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext applicationDbContext)
+        [HttpGet]
+        [Route("test")]
+        public IActionResult Test()
         {
-            this.userManager = userManager;
-            this.signInManager = signInManager;
-            this.roleManager = roleManager;
-            this.applicationDbContext = applicationDbContext;
-        }
-        [HttpGet, Route("getall")]
-        public IActionResult GetAll()
-        {
-            return Ok(userManager.Users);
-        }
-
-        [HttpPost, Route("signin")]
-        public async Task<IActionResult> SignIn(string email)
-        {
-            var user = await userManager.FindByEmailAsync(email);
-           
-            await signInManager.SignInAsync(user, true);
-            return Ok(user.Email);
-        }
-
-        [HttpGet, Route("signout")]
-        public async Task<IActionResult> SignOut()
-        {
-            await signInManager.SignOutAsync();
-
-            return Ok("Logged out");
-        }
-
-        [Authorize(Roles = "Admin")]
-        [HttpPost, Route("remove")]
-        public async Task<IActionResult> Remove(string email)
-        {
-            var user = await userManager.FindByEmailAsync(email);
-
-            return Ok($"User with email: {email} removed");
+            return Ok("This is okay");
         }
     }
 }
