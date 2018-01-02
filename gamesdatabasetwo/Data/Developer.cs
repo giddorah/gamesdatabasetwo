bd48cfcd3140132b3868656dd39139b5d49b7ab7
Johan Spånberg <j.spanberg@live.com>
2018-01-02 10:49:36 +0100

    Added Game, Publisher, Developer

---
+++ gamesdatabasetwo/Data/Developer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gamesdatabasetwo.Data
+{
+    public class Developer
+    {
+        public string Name { get; set; }
+    }
+}
