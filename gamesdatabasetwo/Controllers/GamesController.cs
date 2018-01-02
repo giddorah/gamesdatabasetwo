da50e7568d576bf8912efcfe267b99a269858429
Johan Spånberg <j.spanberg@live.com>
2018-01-02 10:26:10 +0100

    Add project files.

---
+++ gamesdatabasetwo/Controllers/GamesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gamesdatabasetwo.Controllers
+{
+    [Route("api/Games")]
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("test")]
+        public IActionResult Test()
+        {
+            return Ok("This is okay");
+        }
+    }
+}
