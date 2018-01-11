6754678a1e68e08843893e2ed5da0dff2ab555f7
Johan Spånberg <j.spanberg@live.com>
2018-01-11 13:10:48 +0100

    Added methods for Admins/Publishers to add Publishers and Developers

--- gamesdatabasetwo/Data/Enitites/Developer.cs
+++ gamesdatabasetwo/Data/Enitites/Developer.cs
@@ -7,7 +7,6 @@ namespace gamesdatabasetwo.Data
 {
     public class Developer
     {
-        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
---
+++ gamesdatabasetwo/Data/Models/PublisherCreateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gamesdatabasetwo.Data
+{
+    public class PublisherCreateModel
+    {
+        [StringLength(15, ErrorMessage = "Name can not be longer than 25 characters. ")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name can not be empty. ")]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string Name { get; set; }
+    }
+}
