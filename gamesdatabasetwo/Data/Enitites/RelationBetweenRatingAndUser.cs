bb4c0cce1b026cdac5aeb557b3143fa753743681
Johan Spånberg <j.spanberg@live.com>
2018-01-13 20:57:44 +0100

    Started adding functionality for a relations database between users and ratings.

---
+++ gamesdatabasetwo/Data/Enitites/RelationBetweenRatingAndUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gamesdatabasetwo.Data.Enitites
+{
+    public class RelationBetweenRatingAndUser
+    {
+        public int id { get; set; }
+        public string UserId { get; set; }
+        public int RatingId { get; set; }
+    }
+}
