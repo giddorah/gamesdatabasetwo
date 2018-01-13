9a2053b1cbd11f3eac83a090af89089b9e12e41c
Johan Spånberg <j.spanberg@live.com>
2018-01-13 13:33:14 +0100

    Made hiding of online connection string possible through gitignore and appsettings-file

--- gamesdatabasetwo/Startup.cs
+++ gamesdatabasetwo/Startup.cs
@@ -29,6 +29,9 @@ namespace gamesdatabasetwo
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("OnlineConnection")));
+
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
