337040b7d61721a317a98b6883279cb15ec66a14
Johan Spånberg <j.spanberg@live.com>
2018-01-13 15:18:54 +0100

    Better hidden connection strings. Rollback to before this point on startup.cs and gamesdatabasetwo.csproj if this doesn't work on publish.

--- gamesdatabasetwo/Startup.cs
+++ gamesdatabasetwo/Startup.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using gamesdatabasetwo.Data;
+﻿using gamesdatabasetwo.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace gamesdatabasetwo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        string _offline = null;
+        string _online = null;
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder();
+
+            builder.AddUserSecrets<Startup>();    
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,11 +28,15 @@ namespace gamesdatabasetwo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            _offline = Configuration["online"];
+            _online = Configuration["offline"];
 
             services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("OnlineConnection")));
+            options.UseSqlServer(_offline));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(_online));
+
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
@@ -46,8 +52,10 @@ namespace gamesdatabasetwo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
-            DefaultFilesOptions DefaultFile = new DefaultFilesOptions();
+
+            var DefaultFile = new DefaultFilesOptions();
             DefaultFile.DefaultFileNames.Clear();
             DefaultFile.DefaultFileNames.Add("index.html");
 
